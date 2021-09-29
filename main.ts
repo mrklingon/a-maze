eb7cc3abefa92a4a3ea21bb45ef1a980b99d7ec5
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-09-28 19:15:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 function setPix (xp: number, xy: number, color: number) {
     Universe[getSPOT(xp, xy)] = color
 }
+function distGoal (Cx: number, Cy: number) {
+    return Math.sqrt((Cy - Diam) * (Cy - Diam) + (Cx - Diam) * (Cx - Diam))
+}
 function showUNI (xx: number, yy: number) {
     for (let index = 0; index <= 4; index++) {
         for (let index2 = 0; index2 <= 4; index2++) {
@@ -46,7 +49,7 @@ function mkMaze () {
     y = 0
     my = 0
     mx = 0
-    while (mx != Diameter - 1 && my != Diameter - 1) {
+    while (mx != Diam && my != Diam) {
         mx += randint(-1, 1)
         my += randint(-1, 1)
         if (mx < 0) {
@@ -55,11 +58,11 @@ function mkMaze () {
         if (my < 0) {
             my = 0
         }
-        if (mx > Diameter - 1) {
-            mx = Diameter - 1
+        if (mx > Diam) {
+            mx = Diam
         }
-        if (my > Diameter - 1) {
-            my = Diameter - 1
+        if (my > Diam) {
+            my = Diam
         }
         setPix(mx, my, 0)
     }
@@ -75,6 +78,7 @@ function getX (spot: number) {
 }
 let mx = 0
 let my = 0
+let Diam = 0
 let Universe: number[] = []
 let Unisize = 0
 let Diameter = 0
@@ -83,9 +87,10 @@ let x = 0
 x = 0
 y = 0
 let mode = 0
-Diameter = 100
+Diameter = 0 + 0
 Unisize = Diameter * Diameter
 Universe = [0]
+Diam = Diameter - 1
 for (let index = 0; index < Unisize; index++) {
     Universe.push(0)
 }
@@ -97,7 +102,4 @@ loops.everyInterval(500, function () {
         basic.pause(100)
         led.plotBrightness(0, 0, Sav)
     }
-})
-basic.forever(function () {
-	
 })
